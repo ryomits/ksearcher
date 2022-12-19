30177e4611ec9fcc3e44224530235c32d58d3290
mitsui <mitsui@sobuline.com>
2022-12-19 23:14:59 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Head>
+        <title>ksercher</title>
+      </Head>
+      <div>aaa</div>
+    </>
+  );
+}
